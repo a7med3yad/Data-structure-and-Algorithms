bd0b50c93325e67d265c05b52d8824c42eabc67e
Ahmed Ibrahim Ayad <160779925+a7med3yad@users.noreply.github.com>
2024-11-14 17:56:53 +0200

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,58 @@
+public class Stack {
+    private pointer head;
+  // كود الstack بالطريقة العيادية 
+  // using linkedlist
+    int cnt=0;
+    public boolean isEmpty(){
+        return head==null;
+    }
+    public void insert(int n){
+        pointer newpointer= new pointer(n);
+        if(isEmpty()){
+            head=newpointer;
+            cnt++;
+            return;
+        }
+        newpointer.prev=head;
+        head=newpointer;
+        cnt++;
+    }
+    public void pop(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+            return;
+        }
+        if(head.prev ==null && head.next==null){
+            head=null;
+            
+        }
+        else{
+            head=head.prev;
+            head.next=null;
+        }
+        cnt--;
+    }
+    public void print_Stack(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+            return ;
+        }
+        pointer tmp = head;
+        while(tmp!=null){
+            System.out.print(tmp.info+" ");
+            tmp=tmp.prev;
+        }
+        System.out.println();
+    }
+    public void top(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+        }
+        else{
+            System.out.println("Top = "+head.info);
+        }
+    }
+    public int Size(){
+        return cnt;
+    }
+}
