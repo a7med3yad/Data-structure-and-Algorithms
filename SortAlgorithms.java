d604a713028945cfc245122ddada7b059d414e8c
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2025-01-26 00:17:43 +0200

    Update SortAlgorithms.java

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -1,4 +1,37 @@
 public class SortAlgorithms {
+      public static void mergeSort(int arr[], int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+    
+    public static void merge(int arr[], int start, int mid, int end) {
+        int tmp[] = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+    
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                tmp[k++] = arr[i++];
+            } else {
+                tmp[k++] = arr[j++];
+            }
+        }
+    
+        while (i <= mid) {
+            tmp[k++] = arr[i++];
+        }
+    
+        while (j <= end) {
+            tmp[k++] = arr[j++];
+        }
+    
+        for (i = start, k = 0; i <= end; i++, k++) {
+            arr[i] = tmp[k];
+        }
+    }
     public static boolean BinarySearch(int arr[],int target){
         int left=0;
         int right=arr.length;
@@ -48,7 +81,7 @@ public class SortAlgorithms {
         return arr;
     }
     public static void QuickSort(int array[],int start,int end){
-        if(end <= start) return;//base case
+        if(end <= start) return;
         int pivot =partition(array, start, end);
         QuickSort(array,start,pivot-1);
         QuickSort(array,pivot+1,end);
